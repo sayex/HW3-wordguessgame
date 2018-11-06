693124ae9e950eb6f892adab310b7b0b6faf5a8e
sayex <elsayer@msn.com>
2018-11-05 23:22:51 -0700

    can't get innerhtml to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,26 @@
 var words = ["battleship", "chess"];
-var wordarray = [];
-var wordhidden = [];
+var wordArray = [];
+var wordHidden = [];
 var correctLetters = [];
 var wrongGuessLetters = [];
 var wordSelect = words[Math.floor(Math.random() * words.length)];
 
-for (var i = 0; i < wordSelect.length; i++) {
-  wordarray.push(wordSelect.charAt(i));
-  wordhidden.push("_");
-}
-console.log(wordhidden);
-document.getElementById("wordGuess").innerHTML = wordhidden.join(" ");
+//
 
-document.onkeypress = function(event) {
-  var storedLetter = event.key;
-  console.log(storedLetter);
+// for (var i = 0; i < wordSelect.length; i++) {
+//   wordArray.push(wordSelect.charAt(i));
+//   // wordHidden.push("_");
+// }
 
-  for (var i = 0; i < wordarray.length; i++) {
-    if (wordarray[i] === storedLetter) {
-      correctLetters.push(storedLetter);
-      console.log("correct Letter " + correctLetters);
-    }
-  }
-};
+document.getElementById("wordGuess").textContent = wordSelect;
+
+// document.onkeypress = function(event) {
+//   var storedLetter = event.key;
+//   console.log(storedLetter);
+
+//   for (var i = 0; i < wordArray.length; i++) {
+//     if (wordArray[i] === storedLetter) {
+//       correctLetters.push(storedLetter);
+//     }
+//   }
+// };
